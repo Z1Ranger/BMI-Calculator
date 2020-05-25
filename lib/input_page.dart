7d9c3963a19141e5b731e3d982f07e8dd3720caf
Z1Ranger <gvvarma2712@gmail.com>
2020-05-24 23:27:12 -0400

    added gender widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const double bottomContainerHeight = 60;
 const activeColour = Color(0xFF1D1E33);
@@ -25,11 +26,15 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: BasicCard(
                     color: activeColour,
+                    cardChild:
+                        CustomIcon(icon: FontAwesomeIcons.mars, text: 'MALE'),
                   ),
                 ),
                 Expanded(
                   child: BasicCard(
                     color: activeColour,
+                    cardChild: CustomIcon(
+                        icon: FontAwesomeIcons.venus, text: 'FEMALE'),
                   ),
                 ),
               ],
@@ -68,14 +73,46 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class CustomIcon extends StatelessWidget {
+  CustomIcon({@required this.icon, @required this.text});
+
+  final icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 18,
+            color: Color(0xFF8D8E98),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class BasicCard extends StatelessWidget {
-  BasicCard({@required this.color});
+  BasicCard({@required this.color, this.cardChild});
 
   final Color color;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(10),
